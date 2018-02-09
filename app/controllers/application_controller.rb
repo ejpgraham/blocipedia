ee1593fced6cae674ff6d2d165039b749d66512e
ejpgraham <ejpgraham@gmail.com>
2018-02-08 22:06:23 -0500

    Private wikis are only visible by their creators or admins. Private wikis are made public if they lose premium status.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def revert_users_wikis_to_public
+    current_user.wikis.each do |wiki|
+      wiki.update_attribute(:private, false)
+    end
+  end
+
 end
--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -2,8 +2,9 @@ class DowngradeController < ApplicationController
 
   def downgrade
     current_user.update_attribute(:premium, false)
+    revert_users_wikis_to_public if !current_user.admin?
     redirect_to edit_user_registration_path(current_user)
-    flash[:alert] = "Your account has been downgraded from premium to standard"
+    flash[:notice] = "Your account has been downgraded from premium to standard."
   end
 
 end
